844b46ea9862cdb3eee5e94d14a1f5e67bf03a93
sbx_user1051 <sbx_user1051@169.254.14.49>
2023-12-01 20:05:47 +0000

    Fri Dec 01 20:05:47 UTC 2023

--- lib/solutions/SUM/test_sum_solution_alex.py
+++ lib/solutions/SUM/test_sum_solution_alex.py
@@ -19,21 +19,41 @@ django.setup()
 from lib.solutions.SUM.sum_solution_alex import DataAnalyzer  # noqa: E402
 
 
-# Test cases
 class TestDataAnalyzer(TestCase):
     def test_calculate_standard_deviation(self):
-        # Your test logic here
-        self.assertAlmostEqual(DataAnalyzer.calculate_standard_deviation([1, 2, 3, 4, 5]), 1.58, places=2)
+        # Define test cases
+        test_cases = [([1, 2, 3, 4, 5], 1.581), ([10, 10, 10, 10, 10], 0), ([1.5, 2.5, 3.5, 4.5, 5.5], 1.581)]
+
+        # Run and assert tests
+        for data, expected in test_cases:  # TODO shouldnt be loop.
+            with self.subTest(data=data):
+                result = DataAnalyzer.calculate_standard_deviation(data)
+                self.assertAlmostEqual(result, expected, places=3)
 
     def test_calculate_linear_regression(self):
-        # Your test logic here
-        self.assertEqual(DataAnalyzer.calculate_linear_regression([(1, 1), (2, 2), (3, 3)]), (1, 0))
+        # Define test cases
+        test_cases = [
+            ([(1, 1), (2, 2), (3, 3)], (1, 0)),
+            ([(1, 2), (2, 4), (3, 6)], (2, 0)),
+            ([(1, 3), (2, 3), (3, 3)], (0, 3)),
+        ]
 
+        # Run and assert tests
+        for points, expected in test_cases:
+            with self.subTest(points=points):  # Tdodo - not loop
+                slope, intercept = DataAnalyzer.calculate_linear_regression(points)
+                self.assertAlmostEqual(slope, expected[0], places=2)
+                self.assertAlmostEqual(intercept, expected[1], places=2)
 
-# More tests as needed
 
 # Run the tests
 if __name__ == "__main__":
     django.test.utils.setup_test_environment()
     django.test.runner.DiscoverRunner().run_tests(["test_data_analyzer"])
+
+# Run the tests
+if __name__ == "__main__":
+    django.test.utils.setup_test_environment()
+    django.test.runner.DiscoverRunner().run_tests(["test_data_analyzer"])
+
 
