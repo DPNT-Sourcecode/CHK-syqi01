81c0ea9959e0a0891f68a9fd4bc489fef1574a9b
sbx_user1051 <sbx_user1051@169.254.52.141>
2023-12-01 17:39:39 +0000

    Fri Dec 01 17:39:39 UTC 2023

---
+++ alex_project/sum_solution_alex.py
@@ -0,0 +1,10 @@
+# Where:
+#  - param[0] = a positive integer between 0-100
+#  - param[1] = a positive integer between 0-100
+#  - @return = an Integer representing the sum of the two numbers
+
+
+def sum(x: int, y: int) -> int:
+    if not (0 <= x <= 100) or not (0 <= y <= 100):
+        raise ValueError("Arguments must be integers between 1 and 100 (inclusive)")
+    return x + y
