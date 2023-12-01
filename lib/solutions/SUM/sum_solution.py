d1c2a911b7739d16a4fcfb013d187a01d797478a
sbx_user1051 <sbx_user1051@169.254.29.173>
2023-12-01 21:42:18 +0000

    Fri Dec 01 21:42:18 UTC 2023

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,8 +1,9 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 
-from lib.solutions.SUM.sum_solution_alex import sum_lt_100
+from .solution_ref import sum_lt_100
 
 
 def compute(x, y):
     return sum_lt_100(x, y)
     # raise NotImplementedError()
+
