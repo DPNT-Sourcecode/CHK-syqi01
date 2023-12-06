3014fc0bec173a9c8a8d4a407a35c63a3b519e38
sbx_user1051 <sbx_user1051@169.254.29.137>
2023-12-06 17:01:21 +0000

    Wed Dec 06 17:01:21 UTC 2023

--- lib/solutions/HLO/hello_solution_test.py
+++ lib/solutions/HLO/hello_solution_test.py
@@ -3,3 +3,22 @@ from hello_solution import hello
 words = hello("anything")
 print(words)
 
+# test_hello_world.py
+
+# Function to be tested
+# def hello(_):
+# return "Hello, world!"
+
+
+# Test for the function
+def test_hello():
+    assert hello("any string") == "Hello, world!"
+
+
+# This allows pytest to run the test when this file is executed
+if __name__ == "__main__":
+    import pytest
+
+    pytest.main()
+
+
