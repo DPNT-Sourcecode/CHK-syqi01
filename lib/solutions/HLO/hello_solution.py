4ad1fa3077f0f65bbc639f8d1bb64effbed98710
sbx_user1051 <sbx_user1051@169.254.29.137>
2023-12-06 16:53:21 +0000

    Wed Dec 06 16:53:21 UTC 2023

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -1,6 +1,29 @@
+# The requirement for this round is to say hello to the world. (I know, it's not well defined.)
+
+
+# In order to complete the round you need to implement the following method:
+#      hello(String) -> String
+
+# Where:
+#  - param[0] = a String. Ignore for now.
+#  - @return = a String containing a message
+
+
+# def hello(_):
+#     return "Hello, world!"
+
+
+# Example usage
+# print(hello("anything"))  # You can pass any string as the argument
 
 
 # noinspection PyUnusedLocal
 # friend_name = unicode string
 def hello(friend_name):
-    raise NotImplementedError()
+    """
+    #  - param[0] = a String. Ignore for now.
+    #  - @return = a String containing a message
+    """
+    return "Hello, world!"
+    # raise NotImplementedError()
+
