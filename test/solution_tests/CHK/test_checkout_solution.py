21494f1ea81fac4ef9c08d6245c8eef10a987ec7
sbx_user1051 <sbx_user1051@169.254.12.57>
2023-12-07 19:39:37 +0000

    Thu Dec 07 19:39:37 UTC 2023

--- test/solution_tests/CHK/test_checkout_solution.py
+++ test/solution_tests/CHK/test_checkout_solution.py
@@ -3,10 +3,6 @@ import pytest
 from lib.solutions.CHK.checkout_solution import Checkout
 
 
-import pytest
-from lib.solutions.CHK.checkout_solution import Checkout
-
-
 class TestCheckout:
     @pytest.fixture
     def checkout_system(self):
@@ -14,9 +10,17 @@ class TestCheckout:
 
     @pytest.mark.parametrize(
         "skus, expected",
-        [("ABCD", 115), ("AAAA", 180), ("", 0), ("AAAB", 160), ("AB", 80)],
+        [
+            (None, -1),
+            (123, -1),
+            (["A", "B"], -1),
+            ("EFG", -1),
+            ("A" * 1000, -1),  # if you decide to treat overly long strings as invalid
+        ],
     )
-    def test_calculate_price_with_valid_inputs(self, checkout_system, skus, expected):
+    def test_calculate_price_with_breaking_inputs(
+        self, checkout_system, skus, expected
+    ):
         assert checkout_system.calculate_price(skus) == expected
 
     @pytest.mark.parametrize(
@@ -34,6 +38,7 @@ class TestCheckout:
         self, checkout_system, skus, expected
     ):
         assert checkout_system.calculate_price(skus) == expected
+
 
 
 
