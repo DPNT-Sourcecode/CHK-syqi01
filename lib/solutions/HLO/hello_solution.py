cb0732d590553b34beef8a688cc9a8a721fb629a
sbx_user1051 <sbx_user1051@169.254.12.57>
2023-12-07 17:26:16 +0000

    Thu Dec 07 17:26:16 UTC 2023

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -1,11 +1,12 @@
 # noinspection PyUnusedLocal
 # friend_name = unicode string
 def hello(param):
-    # param is a string, for now, we ignore it
-    # prepare the message you wish to return
-    print("Hello " + param)
-    message = "Hello, World!"
-    return message
+    """
+    param is a string, for now, we ignore it
+    prepare the message you wish to return
+    """
+    print(f"Hello {param}")
+    return "Hello, World!"
 
 
 hello("world!")
