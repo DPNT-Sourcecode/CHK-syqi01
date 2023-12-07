be34cb82a870592ae6ca1a032064d5968542fe20
sbx_user1051 <sbx_user1051@169.254.12.57>
2023-12-07 19:42:37 +0000

    Thu Dec 07 19:42:37 UTC 2023

--- test/solution_tests/CHK/test_checkout_solution.py
+++ test/solution_tests/CHK/test_checkout_solution.py
@@ -23,21 +23,6 @@ class TestCheckout:
     ):
         assert checkout_system.calculate_price(skus) == expected
 
-    @pytest.mark.parametrize(
-        "skus, expected",
-        [
-            (None, -1),
-            (123, -1),
-            (["A", "B"], -1),
-            ("EFG", -1),
-            ("A" * 1000, -1),
-            ("", -1),  # Testing for empty string as well
-        ],
-    )
-    def test_calculate_price_with_breaking_inputs(
-        self, checkout_system, skus, expected
-    ):
-        assert checkout_system.calculate_price(skus) == expected
 
 
 
