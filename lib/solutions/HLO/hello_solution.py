4c1530295b09ab2a69613e586f0dec817edcf896
sbx_user1051 <sbx_user1051@169.254.11.165>
2023-12-07 07:14:17 +0000

    Thu Dec 07 07:14:17 UTC 2023

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -4,7 +4,9 @@
 def hello(param):
     # param is a string, for now, we ignore it
     # prepare the message you wish to return
+    print("Hello " + param)
     message = "Hello, World!"
     return message
+
 
 
