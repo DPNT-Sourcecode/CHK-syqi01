a6c87983a4adab4f96e6404532b44f7f844cf2d8
sbx_user1051 <sbx_user1051@169.254.12.57>
2023-12-07 21:13:35 +0000

    Thu Dec 07 21:13:35 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,113 +1,113 @@
-class Checkout:
-    def __init__(self):
-        self.prices = {"A": 50, "B": 30, "C": 20, "D": 15, "E": 40}
-        self.offers = {
-            "A": {"quantity": 3, "price": 130},
-            "B": {"quantity": 2, "price": 45},
-            "E": {"quantity": 2, "free_item": "B"},
-        }
-        self.max_sku_length = 100
+# class Checkout:
+#     def __init__(self):
+#         self.prices = {"A": 50, "B": 30, "C": 20, "D": 15, "E": 40}
+#         self.offers = {
+#             "A": {"quantity": 3, "price": 130},
+#             "B": {"quantity": 2, "price": 45},
+#             "E": {"quantity": 2, "free_item": "B"},
+#         }
+#         self.max_sku_length = 100
 
-    def validate_skus(self, skus):
-        if not isinstance(skus, str) or len(skus) > self.max_sku_length:
-            return False
-        if any(sku not in self.prices for sku in skus):
-            return False
-        if (
-            not skus.isupper() and skus != ""
-        ):  # Ensure all characters are uppercase or it's an empty string
-            return False
-        return True
+#     def validate_skus(self, skus):
+#         if not isinstance(skus, str) or len(skus) > self.max_sku_length:
+#             return False
+#         if any(sku not in self.prices for sku in skus):
+#             return False
+#         if (
+#             not skus.isupper() and skus != ""
+#         ):  # Ensure all characters are uppercase or it's an empty string
+#             return False
+#         return True
 
-    def calculate_price(self, skus):
-        if not self.validate_skus(skus):
-            return -1
-        if skus == "":
-            return 0
+#     def calculate_price(self, skus):
+#         if not self.validate_skus(skus):
+#             return -1
+#         if skus == "":
+#             return 0
 
-        # Split SKUs and count
-        counts = {sku: skus.count(sku) for sku in set(skus)}
+#         # Split SKUs and count
+#         counts = {sku: skus.count(sku) for sku in set(skus)}
 
-        # Calculate total considering all possible offer combinations
-        total = self.calculate_best_price(counts)
+#         # Calculate total considering all possible offer combinations
+#         total = self.calculate_best_price(counts)
 
-        return total
+#         return total
 
-    def calculate_best_price(self, counts):
-        # Recursive function to calculate the best price considering all offers
-        if not counts:
-            return 0
+#     def calculate_best_price(self, counts):
+#         # Recursive function to calculate the best price considering all offers
+#         if not counts:
+#             return 0
 
-        min_total = float("inf")
+#         min_total = float("inf")
 
-        for sku, count in counts.items():
-            new_counts = counts.copy()
-            new_counts[sku] -= 1
-            if new_counts[sku] == 0:
-                del new_counts[sku]
+#         for sku, count in counts.items():
+#             new_counts = counts.copy()
+#             new_counts[sku] -= 1
+#             if new_counts[sku] == 0:
+#                 del new_counts[sku]
 
-            # Price without applying the offer
-            total_price = self.prices[sku] + self.calculate_best_price(new_counts)
-            min_total = min(min_total, total_price)
+#             # Price without applying the offer
+#             total_price = self.prices[sku] + self.calculate_best_price(new_counts)
+#             min_total = min(min_total, total_price)
 
-            # Price with applying the offer (if available)
-            if sku in self.offers:
-                offer = self.offers[sku]
-                if "quantity" in offer and count >= offer["quantity"]:
-                    new_counts_offer = new_counts.copy()
-                    new_counts_offer[sku] -= offer["quantity"] - 1
-                    if new_counts_offer[sku] <= 0:
-                        del new_counts_offer[sku]
+#             # Price with applying the offer (if available)
+#             if sku in self.offers:
+#                 offer = self.offers[sku]
+#                 if "quantity" in offer and count >= offer["quantity"]:
+#                     new_counts_offer = new_counts.copy()
+#                     new_counts_offer[sku] -= offer["quantity"] - 1
+#                     if new_counts_offer[sku] <= 0:
+#                         del new_counts_offer[sku]
 
-                    total_price_offer = offer["price"] + self.calculate_best_price(
-                        new_counts_offer
-                    )
-                    min_total = min(min_total, total_price_offer)
+#                     total_price_offer = offer["price"] + self.calculate_best_price(
+#                         new_counts_offer
+#                     )
+#                     min_total = min(min_total, total_price_offer)
 
-        return min_total
+#         return min_total
 
-    @staticmethod
-    def run_tests():
-        checkout = Checkout()
-        test_cases = [
-            # Round 1 specific tests
-            ("A", 50),
-            ("AA", 100),
-            ("AAA", 130),  # 3A for 130
-            ("AAAA", 180),  # 3A for 130 + 1A
-            # Round 2 specific tests
-            ("AAAAA", 200),  # 5A for 200
-            ("AAAAAA", 250),  # 5A for 200 + 1A
-            ("EE", 80),  # 2E get one B free, but only E's price is counted
-            ("EEB", 80),  # 2E get one B free, B is not charged
-            # Tests combining offers across different items
-            ("AB", 80),  # No offers
-            ("ABB", 95),  # 2B for 45
-            ("ABEE", 120),  # 2E get one B free, only one B charged
-            # Tests for illegal input
-            ("a", -1),
-            ("1", -1),
-            ("A" * 101, -1),  # Exceeding max SKU length
-            # Tests confirming customer-favoring policy
-            ("AAAB", 160),  # 3A for 130 + 1A + 1B
-        ]
+#     @staticmethod
+#     def run_tests():
+#         checkout = Checkout()
+#         test_cases = [
+#             # Round 1 specific tests
+#             ("A", 50),
+#             ("AA", 100),
+#             ("AAA", 130),  # 3A for 130
+#             ("AAAA", 180),  # 3A for 130 + 1A
+#             # Round 2 specific tests
+#             ("AAAAA", 200),  # 5A for 200
+#             ("AAAAAA", 250),  # 5A for 200 + 1A
+#             ("EE", 80),  # 2E get one B free, but only E's price is counted
+#             ("EEB", 80),  # 2E get one B free, B is not charged
+#             # Tests combining offers across different items
+#             ("AB", 80),  # No offers
+#             ("ABB", 95),  # 2B for 45
+#             ("ABEE", 120),  # 2E get one B free, only one B charged
+#             # Tests for illegal input
+#             ("a", -1),
+#             ("1", -1),
+#             ("A" * 101, -1),  # Exceeding max SKU length
+#             # Tests confirming customer-favoring policy
+#             ("AAAB", 160),  # 3A for 130 + 1A + 1B
+#         ]
 
-        passed = 0
-        for skus, expected in test_cases:
-            result = checkout.calculate_price(skus)
-            if result == expected:
-                passed += 1
-            else:
-                print(
-                    f"Test failed for input '{skus}': Expected {expected}, got {result}"
-                )
+#         passed = 0
+#         for skus, expected in test_cases:
+#             result = checkout.calculate_price(skus)
+#             if result == expected:
+#                 passed += 1
+#             else:
+#                 print(
+#                     f"Test failed for input '{skus}': Expected {expected}, got {result}"
+#                 )
 
-        return f"{passed}/{len(test_cases)} tests passed."
+#         return f"{passed}/{len(test_cases)} tests passed."
 
 
-# Run the tests using the static method
-test_results = Checkout.run_tests()
-print(test_results)
+# # Run the tests using the static method
+# test_results = Checkout.run_tests()
+# print(test_results)
 
 
 # TODO 1: Implement more robust input validation using regular expressions to filter out any non-allowed characters.
@@ -192,3 +192,4 @@ def checkout(skus):
 # Where:
 #  - param[0] = a String containing the SKUs of all the products in the basket
 #  - @return = an Integer representing the total checkout value of the items
+
