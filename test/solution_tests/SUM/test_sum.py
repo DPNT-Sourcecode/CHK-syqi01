d00ccc9a8ad2a7a0e98bdcef1b4b9a27710f5a37
sbx_user1051 <sbx_user1051@169.254.12.57>
2023-12-07 17:56:16 +0000

    Thu Dec 07 17:56:16 UTC 2023

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -1,5 +1,6 @@
 # from solutions.SUM import sum_solution
 import pytest
+
 from lib.solutions.SUM.sum_solution import compute
 
 
@@ -7,62 +8,55 @@ class TestSum:
     def test_sum(self):
         assert compute(1, 2) == 3
 
+    # Happy path tests with various realistic test values
+    @pytest.mark.parametrize(
+        "x, y, expected",
+        [
+            pytest.param(1, 1, 2, id="positive_integers"),
+            pytest.param(-1, -1, -2, id="negative_integers"),
+            pytest.param(0, 0, 0, id="zeroes"),
+            pytest.param(1.5, 2.5, 4.0, id="floating_point"),
+            pytest.param(-1.5, 2.5, 1.0, id="mixed_sign_floats"),
+        ],
+        ids=str,
+    )
+    def test_compute_happy_path(x, y, expected):
+        # Act
+        result = compute(x, y)
 
-# Happy path tests with various realistic test values
-@pytest.mark.parametrize(
-    "x, y, expected",
-    [
-        pytest.param(1, 1, 2, id="positive_integers"),
-        pytest.param(-1, -1, -2, id="negative_integers"),
-        pytest.param(0, 0, 0, id="zeroes"),
-        pytest.param(1.5, 2.5, 4.0, id="floating_point"),
-        pytest.param(-1.5, 2.5, 1.0, id="mixed_sign_floats"),
-    ],
-    ids=str,
-)
-def test_compute_happy_path(x, y, expected):
-    # Act
-    result = compute(x, y)
+        # Assert
+        assert result == expected, f"Expected {expected}, got {result}"
 
-    # Assert
-    assert result == expected, f"Expected {expected}, got {result}"
+    # Edge cases
+    @pytest.mark.parametrize(
+        "x, y, expected",
+        [
+            pytest.param(0, 1, 1, id="zero_first_operand"),
+            pytest.param(1, 0, 1, id="zero_second_operand"),
+            pytest.param(1e100, 1e100, 2e100, id="large_values"),
+            pytest.param(-1e100, -1e100, -2e100, id="large_negative_values"),
+        ],
+        ids=str,
+    )
+    def test_compute_edge_cases(x, y, expected):
+        # Act
+        result = compute(x, y)
 
+        # Assert
+        assert result == expected, f"Expected {expected}, got {result}"
 
-# Edge cases
-@pytest.mark.parametrize(
-    "x, y, expected",
-    [
-        pytest.param(0, 1, 1, id="zero_first_operand"),
-        pytest.param(1, 0, 1, id="zero_second_operand"),
-        pytest.param(1e100, 1e100, 2e100, id="large_values"),
-        pytest.param(-1e100, -1e100, -2e100, id="large_negative_values"),
-    ],
-    ids=str,
-)
-def test_compute_edge_cases(x, y, expected):
-    # Act
-    result = compute(x, y)
-
-    # Assert
-    assert result == expected, f"Expected {expected}, got {result}"
-
-
-# Error cases
-@pytest.mark.parametrize(
-    "x, y, expected_exception",
-    [
-        pytest.param(1, "a", TypeError, id="string_second_operand"),
-        pytest.param("a", 1, TypeError, id="string_first_operand"),
-        pytest.param(None, 1, TypeError, id="none_first_operand"),
-        pytest.param(1, None, TypeError, id="none_second_operand"),
-    ],
-    ids=str,
-)
-def test_compute_error_cases(x, y, expected_exception):
-    # Act & Assert
-    with pytest.raises(expected_exception):
-        compute(x, y)
-
-
-
-
+    # Error cases
+    @pytest.mark.parametrize(
+        "x, y, expected_exception",
+        [
+            pytest.param(1, "a", TypeError, id="string_second_operand"),
+            pytest.param("a", 1, TypeError, id="string_first_operand"),
+            pytest.param(None, 1, TypeError, id="none_first_operand"),
+            pytest.param(1, None, TypeError, id="none_second_operand"),
+        ],
+        ids=str,
+    )
+    def test_compute_error_cases(x, y, expected_exception):
+        # Act & Assert
+        with pytest.raises(expected_exception):
+            compute(x, y)
