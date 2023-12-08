eb2847602981b954499bf34ad83865f19c25bf1b
sbx_user1051 <sbx_user1051@169.254.47.221>
2023-12-08 00:47:43 +0000

    Fri Dec 08 00:47:43 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,5 +1,113 @@
-# Stage 5 - Redo code with robust handling for breaking inputs
+class Checkout:
+    def __init__(self):
+        self.prices = {"A": 50, "B": 30, "C": 20, "D": 15, "E": 40}
+        self.offers = {
+            "A": {"quantity": 3, "price": 130},
+            "B": {"quantity": 2, "price": 45},
+            "E": {"quantity": 2, "free_item": "B"},
+        }
+        self.max_sku_length = 100
 
+    def validate_skus(self, skus):
+        if not isinstance(skus, str) or len(skus) > self.max_sku_length:
+            return False
+        if any(sku not in self.prices for sku in skus):
+            return False
+        if (
+            not skus.isupper() and skus != ""
+        ):  # Ensure all characters are uppercase or it's an empty string
+            return False
+        return True
+
+    def calculate_price(self, skus):
+        if not self.validate_skus(skus):
+            return -1
+        if skus == "":
+            return 0
+
+        total = 0
+        counts = {sku: skus.count(sku) for sku in set(skus)}
+
+        # Handle special offer for E before iterating
+        if "E" in counts:
+            free_bs = counts["E"] // 2
+            counts["B"] = max(0, counts.get("B", 0) - free_bs)
+
+        for sku, count in counts.items():
+            if sku == "A":
+                total += (
+                    (count // 5) * 200
+                    + ((count % 5) // 3) * 130
+                    + ((count % 5) % 3) * self.prices[sku]
+                )
+            elif (
+                sku in self.offers
+                and "quantity" in self.offers[sku]
+                and "price" in self.offers[sku]
+            ):
+                offer = self.offers[sku]
+                total += (count // offer["quantity"]) * offer["price"] + (
+                    count % offer["quantity"]
+                ) * self.prices[sku]
+            else:
+                total += count * self.prices[sku]
+
+        return total
+
+    @staticmethod
+    def run_tests():
+        checkout = Checkout()
+        test_cases = [
+            ("", 0),
+            ("A", 50),
+            ("B", 30),
+            ("C", 20),
+            ("D", 15),
+            ("E", 40),
+            ("a", -1),
+            ("-", -1),
+            ("ABCa", -1),
+            ("AxA", -1),
+            ("ABCDE", 155),
+            ("AA", 100),
+            ("AAA", 130),
+            ("AAAA", 180),
+            ("AAAAA", 200),
+            ("AAAAAA", 250),
+            ("AAAAAAA", 300),
+            ("AAAAAAAA", 330),
+            ("AAAAAAAAA", 380),
+            ("AAAAAAAAAA", 400),
+            ("EE", 80),
+            ("EEB", 80),
+            ("EEEB", 120),
+            ("EEEEBB", 160),
+            ("BEBEEE", 160),
+            ("BB", 45),
+            ("BBB", 75),
+            ("BBBB", 90),
+            ("ABCDEABCDE", 280),
+            ("CCADDEEBBA", 280),
+            ("AAAAAEEBAAABB", 455),
+            ("ABCDECBAABCABBAAAEEAA", 665),
+        ]
+
+        passed = 0
+        for skus, expected in test_cases:
+            result = checkout.calculate_price(skus)
+            if result == expected:
+                passed += 1
+            else:
+                print(
+                    f"Test failed for input '{skus}': Expected {expected}, got {result}"
+                )
+
+        return f"{passed}/{len(test_cases)} tests passed."
+
+
+# checkout_test = Checkout()
+# test_results = checkout_test.run_tests()
+# print(test_results)
 
 # TODO 1: Implement more robust input validation using regular expressions to filter out any non-allowed characters.
 # TODO 2: Refactor the calculate_price method to reduce its complexity and improve readability.
@@ -83,3 +191,4 @@ def checkout(skus):
 # Where:
 #  - param[0] = a String containing the SKUs of all the products in the basket
 #  - @return = an Integer representing the total checkout value of the items
+
